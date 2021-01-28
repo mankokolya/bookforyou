0c4c6b2b5e5fd870d8f80af319a9f9cc4c45e8ad
Mykola Manko <mankokolya@gmail.com>
2021-01-28 23:36:00 +0200

    add search input

--- src/main/java/com/library/bookforyou/controllers/MainController.java
+++ src/main/java/com/library/bookforyou/controllers/MainController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -29,6 +30,10 @@ public class MainController {
         return goHome(model);
     }
 
+    @PostMapping("/login")
+    public String success(Model model) {
+        return goHome(model);
+    }
 
     @GetMapping("/home")
     public String goHome(Model model) {

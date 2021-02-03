4b00a18ddb63c3448d88b9f8685fb169690cf7fc
mankokolya <mankokolya@gmail.com>
2021-02-03 16:49:11 +0200

    adding new librarian

---
+++ src/main/java/com/library/bookforyou/controllers/LibrarianController.java
@@ -0,0 +1,13 @@
+package com.library.bookforyou.controllers;
+
+import com.library.bookforyou.web.dto.userDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/librarian")
+public class LibrarianController {
+
+}
